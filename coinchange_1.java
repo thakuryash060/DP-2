2db7f07ee5b7341027fe9f168d5e35a6f1aa32af
Yash Thakur <144375330+thakuryash060@users.noreply.github.com>
2025-06-29 15:02:05 +0530

    Create coinchange_1.java
    TC:2^amount
    SC:O(amount)

---
+++ coinchange_1.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int change(int amount, int[] coins) {
+        if(coins==null || coins.length==0){
+            return 0;
+        }
+        return recurse(coins,amount,0);
+    }
+    private int recurse(int coins[],int amount,int index){
+        if(index==coins.length || amount<0){
+            return 0;
+        }
+
+        if(amount==0){
+            return 1;
+        }
+
+        int case1=recurse(coins,amount,index+1);
+        int case2=recurse(coins,amount-coins[index],index);
+        return case1+case2;
+    }
+}
